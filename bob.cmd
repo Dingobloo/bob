95080a49b8515aa23490cbb7af40c0c7b19b7a5c
Ross Brackstone <rbrackstone@gmail.com>
2018-05-16 13:13:40 +1000

    Hacks i've been adding to bob, now works on OSX terminal, Bash and command prompt, windows version finds and builds bob with VC but not project yet

--- bob.cmd
+++ bob.cmd
@@ -1,43 +1,64 @@
-#!/bin/bash
-#
-# This is a sketch of an experimental design for a build system where project definitions are written in C.
-# The build system is packaged as a single shell script which contains the C library code as a heredoc string.
-# The shell script creates a concatenation from the library code and the file specified as the first command line argument.
-# Then that is compiled and the result is run, which takes any required build-related actions; for this toy implementation,
-# the only build action is running the C compiler directly, but it could also generate makefiles or VS solutions like premake.
-#
-# Note that using some tricks you can write a single file that is both a valid Unix shell script and Windows batch file,
-# which combined with cross-platform code for the C library would allow a portable single-file build system solution.
-#
-# Example usage:
-#
-# Suppose you have a C project with source files bar.c and baz.c and you want to build an executable called foo.
-#
-# Create foo.bob:
-#
-# void setup() {
-#     c_executable("foo");
-#     sources("bar.c", "baz.c");
-# }
-#
-# Command line transcript:
-# 
-# $ ./bob foo
-# [PROJECT] foo
-#           Compiling C executable 'foo'
-# [COMMAND] gcc -o foo bar.c baz.c
-#
-# $ ./foo
-# STUFF
+:;#!/bin/bash
+:;#
+:;# This is a sketch of an experimental design for a build system where project definitions are written in C.
+:;# The build system is packaged as a single shell script which contains the C library code as a heredoc string.
+:;# The shell script creates a concatenation from the library code and the file specified as the first command line argument.
+:;# Then that is compiled and the result is run, which takes any required build-related actions; for this toy implementation,
+:;# the only build action is running the C compiler directly, but it could also generate makefiles or VS solutions like premake.
+:;#
+:;# Note that using some tricks you can write a single file that is both a valid Unix shell script and Windows batch file,
+:;# which combined with cross-platform code for the C library would allow a portable single-file build system solution.
+:;#
+:;# Example usage:
+:;#
+:;# Suppose you have a C project with source files bar.c and baz.c and you want to build an executable called foo.
+:;#
+:;# Create foo.bob:
+:;#
+:;# void setup() {
+:;#     c_executable("foo");
+:;#     sources("bar.c", "baz.c");
+:;# }
+:;#
+:;# Command line transcript:
+:;# 
+:;# $ ./bob.cmd foo
+:;# [PROJECT] foo
+:;#           Compiling C executable 'foo'
+:;# [COMMAND] gcc -o foo bar.c baz.c
+:;#
+:;# $ ./foo
+:;# STUFF
 
-bob_c=$(cat <<END
-#line 34 "bob"
+:<<"::CMDLITERAL"
+@echo off
+
+setlocal
+
+set tmp_c=%TEMP%\bob-c.%random%
+
+::CMDLITERAL
+
+
+:;# Implement goto as a no-op in Shell because it simplifies some escaping.
+:;function goto(){
+    :;
+:;}
+
+:;# What follows has to be on the same line because anything aftwards gets put in the C code
+:;# Shell sees the CMD function call as a line-broken herestring up to the &&
+:;# CMD sees the Shell code as an end of line comment
+:;# Both are able to share the :END label to end the C code block.
+
+:<<<\
+call :heredoc bobc %tmp_c% && goto END &:; bob_c=$(cat <<:END
+#line 55 "bobdirty.cmd"
 
 #include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+#include <iso646.h>
 char *strf(char *fmt, ...) {
     va_list args;
     va_start(args, fmt);
@@ -49,20 +70,19 @@ char *strf(char *fmt, ...) {
     va_end(args);
     return str;
 }
-
 void vmsg(char *kind, char *fmt, va_list args) {
     printf("%s%*s", kind, (int)(strlen(kind) <= 10 ? 10 - strlen(kind) : 1), "");
     vprintf(fmt, args);
+    //This newline character causes compile errors on a shell with stricter posix compliance
+    //ie. calling the script with sh (bash doesnt complain)
     printf("\n");
 }
-
 void msg(char *kind, char *fmt, ...) {
     va_list args;
     va_start(args, fmt);
     vmsg(kind, fmt, args);
     va_end(args);
 }
-
 void error(char *fmt, ...) {
     va_list args;
     va_start(args, fmt);
@@ -70,31 +90,26 @@ void error(char *fmt, ...) {
     va_end(args);
     exit(1);
 }
-
 void info(char *fmt, ...) {
     va_list args;
     va_start(args, fmt);
     vmsg("", fmt, args);
     va_end(args);
 }
-
 int run(char *str) {
     msg("[COMMAND]", "%s", str);
     return system(str);
 }
-
 typedef struct {
     char *data;
     size_t num_items;
     size_t max_items;
 } array_t;
-
 void array_init(array_t *array, size_t item_size) {
     array->num_items = 0;
     array->max_items = 16;
     array->data = malloc(array->max_items * item_size);
 }
-
 void array_add(array_t *array, void *item_ptr, size_t item_size) {
     if (array->num_items == array->max_items) {
         array->max_items *= 2;
@@ -103,7 +118,6 @@ void array_add(array_t *array, void *item_ptr, size_t item_size) {
     memcpy(array->data + array->num_items*item_size, item_ptr, item_size);
     array->num_items++;
 }
-
 typedef struct {
     union {
         struct {
@@ -113,32 +127,26 @@ typedef struct {
         array_t files_array;
     };
 } file_list_t;
-
 void file_list_init(file_list_t *file_list) {
     array_init(&file_list->files_array, sizeof(char *));
 }
-
 void file_list_add(file_list_t *file_list, char *filename) {
     array_add(&file_list->files_array, &filename, sizeof(char *));
 }
-
 enum {
     NONE,
     C_EXECUTABLE
 };
-
 typedef struct {
     int type;
     char *name;
     file_list_t sources;
 } project_t;
-
 void project_init(project_t *project) {
     project->type = NONE;
     project->name = "<unnamed>";
     file_list_init(&project->sources);
 }
-
 void project_build(project_t *project) {
     if (project->type == NONE) error("Trying to build project '%s' with no type selected.", project->name);
     msg("[PROJECT]", "%s", project->name);
@@ -148,21 +156,18 @@ void project_build(project_t *project) {
         char *cmd = strf("gcc -o %s", project->name);
         for (size_t i = 0; i < project->sources.num_files; i++)
             cmd = strf("%s %s", cmd, project->sources.files[i]);
-        if (run(cmd) != 0) error("Compilation failed.");
+        // Uses iso646.h to avoid the not equal operator which interferes with CMD delayed expansion
+        if (run(cmd) not_eq 0) error("Compilation failed.");
     }
 }
-
 project_t *this_project;
-
 void c_executable(char *name) {
     this_project->type = C_EXECUTABLE;
     this_project->name = name;
 }
-
 void source(char *file) {
     file_list_add(&this_project->sources, file);
 }
-
 void sources_func(char *file, ...) {
     va_list args;
     va_start(args, file);
@@ -172,12 +177,14 @@ void sources_func(char *file, ...) {
     }
     va_end(args);
 }
-
 #define sources(...) sources_func(__VA_ARGS__, NULL)
-
 void setup();
-
 int main(int argc, char **argv) {
+    int a;
+    printf("Press any key when debugger is attached: ");
+
+    scanf("%d",&a);
+
     project_t default_project;
     project_init(&default_project);
     this_project = &default_project;
@@ -186,24 +193,130 @@ int main(int argc, char **argv) {
     return 0;
 }
 
-END
+:END
 )
+
+:;# CMD skips to it's own processing from here and Shell ignores it
+goto CMDSCRIPT
+
+   #########################################
+   ## Shell Script                        ##
+   #########################################
 
 set -e
 
 function cleanup {
-    rm -f $tmp_c $tmp_exe
+  echo Removing temp files
+  rm -f $tmp_c $tmp_exe
 }
 
 trap cleanup EXIT
 
-tmp_c=$(mktemp /tmp/bob-XXXXX.c)
-tmp_exe=${tmp_c%.c}
+echo Creating Temp C file
+
+#OSX mktemp (At least as of 10.10.5 Yosemite) fails to substitute if the file template also has an extension
+#also has no --suffix option, Randomising the extension and telling gcc it's a c file was simplest work around.
+tmp_c=$(mktemp /tmp/bob-c.XXXXX)
+
+echo "$tmp_c"
+
+echo Naming Executable
+#tmp_exe=${tmp_c%.c}
+
+#Due to the above suffix issues with OSX mktemp we can't base the exe name on the c file
+tmp_exe=$(mktemp /tmp/bob-exe.XXXXX)
 
 echo "$bob_c" >&$tmp_c
 echo "#line 0 \"$1.bob\"" >>$tmp_c
 cat $1.bob >>$tmp_c
 
-gcc -std=c99 -o $tmp_exe $tmp_c
+gcc -std=c99 -ggdb -o $tmp_exe -x c $tmp_c
 
 eval $tmp_exe
+
+exit 0
+
+:: #########################################
+:: ## Command Prompt Script               ##
+:: #########################################
+
+:CMDSCRIPT
+
+echo #line 0 "%1.bob" >>%tmp_c%
+type %1.bob >>%tmp_c%
+
+set tmp_exe=%TEMP%\bob-exe.%random%.exe
+
+::gcc -std=c99 -o %tmp_exe% -x c %tmp_c%
+
+::Use vswhere to get latest installed visual studio directory
+
+set vswhere_path="%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\"
+
+pushd %CD%
+cd %vswhere_path%
+
+for /f "usebackq delims=" %%i in (`vswhere.exe -latest -property installationPath`) do (
+	set vcpath=%%i
+)
+
+popd 
+echo Found VC: %vcpath%
+
+	::Work around the fact that VC 2017 vcvarsall is an alias for the Dev command prompt and messes with working directory
+	pushd %CD%
+
+		::call "D:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat"
+
+		::call "I:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\Tools\VsDevCmd.bat" 
+
+		::call "I:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x64 >nul 2>&1
+		
+		call "%vcpath%\VC\Auxiliary\Build\vcvarsall.bat" x64 >nul 2>&1
+
+		if %errorlevel% NEQ 0 echo Problem configuring VC environment
+	popd
+
+	echo calling compiler
+
+cl /Tc %tmp_c% /Fe%tmp_exe% /nologo -Zi
+
+echo %tmp_exe%
+
+%tmp_exe%
+
+del %tmp_c%
+del %tmp_exe%
+
+endlocal
+
+:: End of main script
+goto :EOF
+
+:: ########################################
+:: ## Heredoc processing code ##
+:: ########################################
+:invokedoc <uniqueIDX>
+setlocal enabledelayedexpansion
+set go=
+for /f "delims=" %%A in ('findstr /n "^" "%~f0"') do (
+    set "line=%%A" && set "line=!line:*:=!"
+    if defined go (if #!line:~1!==#!go::=! (goto :EOF) else echo(!line!)
+    if "!line:~0,13!"=="call :heredoc" (
+        for /f "tokens=3 delims=>^ " %%i in ("!line!") do (
+            if #%%i==#%1 (
+                for /f "tokens=2 delims=&" %%I in ("!line!") do (
+                    for /f "tokens=2" %%x in ("%%I") do set "go=%%x"
+                )
+            )
+        )
+    )
+)
+exit /B
+
+::A wrapping function so we do not expose Shell to IO redirection on the single line
+:heredoc <iniqueIDX> <filename>
+  setlocal  
+  call :invokedoc %1 > %~2
+  endlocal
+exit /B
